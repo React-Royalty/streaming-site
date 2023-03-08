6e978421f66aa61c6b3abfc420cbac787dd0eded
Andrew Cook <andrewjcook93@gmail.com>
2023-03-08 13:59:27 -0600

    possible logo and title

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
-import './css/style.css'
+import '../css/style.css'
 
 function App() {
   return (
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="home">
+
+      <h1>marathon</h1>
+
+      <br />
+
+      <h1>m</h1>
+
+    </div>
   )
 }
 
